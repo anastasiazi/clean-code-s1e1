f5564bbfa794308d81f220c21b7752d8ed68c7fa
anastasiazi <anastasiiiazi@gmail.com>
2021-04-06 12:36:13 +0300

    refactor: adjust HTML- and CSS-formatting according to rule 3.3 in  html-and-css.md

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox_input"
     editInput.type="text";
     editInput.className="task input text_input";
 
@@ -45,6 +46,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="icon_btn"
     deleteButton.appendChild(deleteButtonImg);
 
 
